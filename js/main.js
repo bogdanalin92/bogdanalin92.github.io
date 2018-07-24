743ea4f094b3b4747ede81ceb92d1f2834b55a43
bogdanalin92 <bogdanalin92@gmail.com>
2018-07-24 12:55:34 +0300

    Update main.js
    Minified content.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,1 @@
-/*! jQuery v3.3.1 | (c) JS Foundation and other contributors | jquery.org/license */ 
-$(document).ready(function(){
-      $("#bodyPage").load("main.html"); 
- });
+$(document).ready(function(){$("#bodyPage").load("main.html")});
