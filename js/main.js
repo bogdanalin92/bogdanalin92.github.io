3380450de18adf79fcee1aec76b980e7de982cad
bogdanalin92 <bogdanalin92@gmail.com>
2018-07-24 12:41:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
- $(document).ready(function(){
+/*! jQuery v3.3.1 | (c) JS Foundation and other contributors | jquery.org/license */ 
+$(document).ready(function(){
       $("#bodyPage").load("main.html"); 
  });
