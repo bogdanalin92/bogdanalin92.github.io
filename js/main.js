18b4b8dff284286cc560a4f74dedb18c38168d1f
bogdanalin92 <bogdanalin92@gmail.com>
2018-07-30 09:45:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(document).ready(
    function(){
       $("#item1").load($basehtml+"/main.html");
       $("#item2").load($basehtml+"/htmlTuts.html");
-   };
-   function(e) oncliks{
-      alert(e);
-   };
-);
+   };);
+function oncliks(e){
+   alert(e);
+};
+
