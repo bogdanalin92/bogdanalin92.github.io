2244c9226ae4f82e8dd019e5d25df076ef218e06
bogdanalin92 <bogdanalin92@gmail.com>
2018-07-30 09:50:34 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(
    function(){
       $("#item1").load($basehtml+"/main.html");
       $("#item2").load($basehtml+"/htmlTuts.html");
-   };);
+   });
 function oncliks(e){
    alert(e);
 };
