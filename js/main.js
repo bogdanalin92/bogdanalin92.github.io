e73b9cc6b0dc76ead932d8b1e54e53e74dec3e03
bogdanalin92 <bogdanalin92@gmail.com>
2018-07-30 09:42:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $basehtml = "/html";
-$(document).ready(function(){
-   $("#item1").load($basehtml+"/main.html");
-   $("#item2").load($basehtml+"/htmlTuts.html");
-});
+$(document).ready(
+   function(){
+      $("#item1").load($basehtml+"/main.html");
+      $("#item2").load($basehtml+"/htmlTuts.html");
+   };
+   function oncliks(e){
+      alert(e);
+   };
+);
